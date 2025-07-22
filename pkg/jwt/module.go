e8488782e72b852c649f6e746b21d7033b465f25
Rishabh Nrupnarayan <aryannr97@gmail.com>
2025-07-22 21:54:00 +0530

    feat: added jwt pkg

---
+++ pkg/jwt/module.go
@@ -0,0 +1,13 @@
+package jwt
+
+// CommandModule represents the collection of different command configs
+type CommandModule struct {
+	CommandDecodeConfig commandDecodeConfig
+}
+
+// NewCommandModule returns the command module
+func NewCommandModule() *CommandModule {
+	return &CommandModule{
+		CommandDecodeConfig: fetchCommandDecodeConfig(),
+	}
+}
