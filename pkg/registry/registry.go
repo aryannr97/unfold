20a1b81d29a55f60c32686256f5c0f67797cce2b
Rishabh Nrupnarayan <aryannr97@gmail.com>
2025-07-22 21:56:41 +0530

    feat: added registry pkg

---
+++ pkg/registry/registry.go
@@ -0,0 +1,42 @@
+package registry
+
+import (
+	"flag"
+
+	"github.com/aryannr97/unfold/pkg/azure"
+	"github.com/aryannr97/unfold/pkg/base64"
+	"github.com/aryannr97/unfold/pkg/commands"
+	"github.com/aryannr97/unfold/pkg/google"
+	"github.com/aryannr97/unfold/pkg/jwt"
+)
+
+// Operation represents the operation to be executed
+type Operation interface {
+	Execute() string
+	GetFlagSet() *flag.FlagSet
+}
+
+// Registry represents the commands to be executed
+type Registry map[string]map[string]Operation
+
+// New returns the list of commands to be executed
+func New() Registry {
+	return Registry{
+		commands.Azure: {
+			commands.Get:       azure.NewCommandModule().CommandGetConfig,
+			commands.Search:    azure.NewCommandModule().CommandSearchConfig,
+			commands.Configure: azure.NewCommandModule().CommandConfigureConfig,
+		},
+		commands.Google: {
+			commands.Get:       google.NewCommandModule().CommandGetConfig,
+			commands.Search:    google.NewCommandModule().CommandSearchConfig,
+			commands.Configure: google.NewCommandModule().CommandConfigureConfig,
+		},
+		commands.JWT: {
+			commands.Decode: jwt.NewCommandModule().CommandDecodeConfig,
+		},
+		commands.Base64: {
+			commands.Decode: base64.NewCommandModule().CommandDecodeConfig,
+		},
+	}
+}
