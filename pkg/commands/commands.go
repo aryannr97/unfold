8eaa3cda81f0a6951af608f499c23bdeb81122e7
Rishabh Nrupnarayan <aryannr97@gmail.com>
2025-07-22 21:56:24 +0530

    feat: added helpers pkg

---
+++ pkg/commands/commands.go
@@ -0,0 +1,15 @@
+package commands
+
+const (
+	// Commands
+	Azure  = "azure"
+	Google = "google"
+	JWT    = "jwt"
+	Base64 = "base64"
+
+	// Sub-commands
+	Get       = "get"
+	Search    = "search"
+	Configure = "configure"
+	Decode    = "decode"
+)
